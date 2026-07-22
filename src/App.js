cd48d217f012bacc06d3f6da1f31352423063e82
lmoramaga <linahmoramaga@gmail.com>
2026-07-22 13:08:01 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./SheCodes logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo img-fluid" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          <footer className="App-footer">Coded by Lina Moramaga</footer>
+        </small>
+      </div>
     </div>
   );
 }
